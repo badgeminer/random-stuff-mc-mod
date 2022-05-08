3ab7c55118f5efb84b26ab2f479d742846520fea
badgeminer <64038298+badgeminer@users.noreply.github.com>
2022-05-08 17:54:24 -0600

    ffg

--- src/main/java/net/mcreator/randomstuff/client/renderer/NoobRenderer.java
+++ src/main/java/net/mcreator/randomstuff/client/renderer/NoobRenderer.java
@@ -1,16 +1,20 @@
 
 package net.mcreator.randomstuff.client.renderer;
 
-public class NoobRenderer extends MobRenderer<NoobEntity, Modelsteve<NoobEntity>> {
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
 
+import net.mcreator.randomstuff.entity.NoobEntity;
+import net.mcreator.randomstuff.client.model.Modelsteve;
+
+public class NoobRenderer extends MobRenderer<NoobEntity, Modelsteve<NoobEntity>> {
 	public NoobRenderer(EntityRendererProvider.Context context) {
 		super(context, new Modelsteve(context.bakeLayer(Modelsteve.LAYER_LOCATION)), 0.5f);
-
 	}
 
 	@Override
 	public ResourceLocation getTextureLocation(NoobEntity entity) {
 		return new ResourceLocation("random_stuff:textures/noob-1234-20274971.png");
 	}
-
 }
--- src/main/java/net/mcreator/randomstuff/procedures/NoobEntityDiesProcedure.java
+++ src/main/java/net/mcreator/randomstuff/procedures/NoobEntityDiesProcedure.java
@@ -1,8 +1,8 @@
 package net.mcreator.randomstuff.procedures;
 
-import net.minecraftforge.eventbus.api.Event;
-
-import javax.annotation.Nullable;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.core.BlockPos;
 
 public class NoobEntityDiesProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z) {
