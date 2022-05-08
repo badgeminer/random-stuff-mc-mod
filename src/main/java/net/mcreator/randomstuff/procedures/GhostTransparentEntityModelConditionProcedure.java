91f8d4dc577f2dc96444114770057512ef2c6f93
105132084+randomstuffmod@users.noreply.github.com <steph@10.0.0.160>
2022-05-08 12:09:29 -0600

    fexxxxxxxx

--- src/main/java/net/mcreator/randomstuff/procedures/GhostTransparentEntityModelConditionProcedure.java
+++ src/main/java/net/mcreator/randomstuff/procedures/GhostTransparentEntityModelConditionProcedure.java
@@ -1,9 +1,5 @@
 package net.mcreator.randomstuff.procedures;
 
-import net.minecraftforge.eventbus.api.Event;
-
-import javax.annotation.Nullable;
-
 public class GhostTransparentEntityModelConditionProcedure {
 	public static boolean execute() {
 		return true;
