dbae4eed1485917c2c9ff258c870c3377a1ea6da
105132084+randomstuffmod@users.noreply.github.com <steph@10.0.0.160>
2022-05-13 22:19:33 -0600

    SDFG

--- src/main/java/net/mcreator/randomstuff/procedures/BobCommandExecutedProcedure.java
+++ src/main/java/net/mcreator/randomstuff/procedures/BobCommandExecutedProcedure.java
@@ -90,6 +90,11 @@ public class BobCommandExecutedProcedure {
 			if (_mcserv != null)
 				_mcserv.getPlayerList().broadcastMessage(new TextComponent("bob"), ChatType.SYSTEM, Util.NIL_UUID);
 		}
+		if (!world.isClientSide()) {
+			MinecraftServer _mcserv = ServerLifecycleHooks.getCurrentServer();
+			if (_mcserv != null)
+				_mcserv.getPlayerList().broadcastMessage(new TextComponent("bob"), ChatType.SYSTEM, Util.NIL_UUID);
+		}
 		if (world instanceof Level _level) {
 			if (!_level.isClientSide()) {
 				_level.playSound(null, new BlockPos(x, y, z),
